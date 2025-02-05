4afc3aef8136f3131d20176b69bf89a5b864b6da
Youcef Derder <youde067@gmail.com>
2025-02-05 23:33:33 +0000

    Modify cleanup script to remove orphan SGs remaining from the created ELB from the ingress controller

--- cleanup.sh
+++ cleanup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Delete Helm releases
-helm uninstall argo-cd --namespace eks-argocd
+helm uninstall argocd-release --namespace eks-argocd
 helm uninstall cert-manager --namespace eks-cert-manager 
 helm uninstall external-dns --namespace eks-external-dns 
-helm uninstall nginx-ingress --namespace ingress-controller
+helm uninstall nginx-ingress-controller --namespace ingress-controller
 
 # Delete ArgoCD related CRDs
-kubectl delete crd applications.argoproj.io \
+kubectl delete crds applications.argoproj.io \
     applicationsets.argoproj.io \
     appprojects.argoproj.io
 
@@ -18,6 +18,14 @@ kubectl delete crd certificaterequests.cert-manager.io \
    issuers.cert-manager.io \
    orders.acme.cert-manager.io
 
-# Optional: Delete namespaces if no longer needed
+# Delete namespaces if no longer needed
 kubectl delete namespace eks-argocd
 kubectl delete namespace eks-cert-manager
+
+# Delete orphan ELB Security Groups
+
+for sg in $(aws ec2 describe-security-groups --query "SecurityGroups[?contains(GroupName, 'k8s-elb-')].GroupId" --output text); 
+do
+echo "Deleting Security Group: $sg"
+aws ec2 delete-security-group --group-id $sg
+done
